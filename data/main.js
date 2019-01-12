a3bca9df76f086f253c72034f185c968e6aa8832
Dario Leunig <daleunig@gmail.com>
2019-01-12 13:56:49 +0100

    collect_cat_amount() function add

--- data/main.js
+++ data/main.js
@@ -113,3 +113,41 @@ function collect_rfid() {
 		};
 	}
 }
+
+function collect_cat_amount() {
+	var configurationData = new FormData();
+
+	configurationElements = document.body.querySelectorAll('*[amount-config]');
+	for (var i = configurationElements.length - 1; i >= 0; i--) {
+	  var configurationKey = configurationElements[i].getAttribute("amount-config");
+	  var configurationValue = configurationElements[i].value;
+	  configurationData.append(configurationKey, configurationValue);
+
+	if (getQueryVariable("c1_uid")) {
+		configurationData.append("c1_uid", getQueryVariable("c1_uid"));
+	}
+	if (getQueryVariable("c2_uid")) {
+		configurationData.append("c2_uid", getQueryVariable("c2_uid"));
+	}
+	if (getQueryVariable("c3_uid")) {
+		configurationData.append("c3_uid", getQueryVariable("c3_uid"));
+	}
+	if (getQueryVariable("c4_uid")) {
+		configurationData.append("c4_uid", getQueryVariable("c4_uid"));
+	}
+
+	if (getQueryVariable("c1_uid") || getQueryVariable("c2_uid") || getQueryVariable("c3_uid") || getQueryVariable("c4_uid")) {
+		var request = new XMLHttpRequest();
+		request.addEventListener("load", transferComplete);
+		request.addEventListener("error", transferFailed);
+		request.open("POST", "/set_amount");
+		console.log("Sending amount");
+		request.send(configurationData);
+		function transferFailed() {
+		alert("Es ist ein Fehler aufgetreten, bitte versuchen Sie es erneut.");
+		}
+		function transferComplete() {
+		alert("Die Futtermenge wurde erfolgreich gespeichert.");
+		};
+	}
+}}
