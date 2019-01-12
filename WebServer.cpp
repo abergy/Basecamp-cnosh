b7e372f86c85dac3d311fad23531cb927bbdca51
Alex <alex.bergmann87@gmail.com>
2019-01-12 18:34:12 +0100

    extract all routes belong to cnosh

--- WebServer.cpp
+++ WebServer.cpp
@@ -104,12 +104,6 @@ void WebServer::begin(Configuration &configuration,
         response->addHeader("Content-Encoding", "gzip");
         request->send(response);
     });
-    server.on("/cnosh.js", HTTP_GET, [](AsyncWebServerRequest *request) {
-        AsyncWebServerResponse *response = request->beginResponse_P(
-            200, "text/js", cnosh_js_gz, cnosh_js_gz_len);
-        response->addHeader("Content-Encoding", "gzip");
-        request->send(response);
-    });
     server.on("/logo.png", HTTP_GET, [](AsyncWebServerRequest *request) {
         AsyncWebServerResponse *response = request->beginResponse_P(
             200, "image/png", logo_png_gz, logo_png_gz_len);
@@ -164,33 +158,6 @@ void WebServer::begin(Configuration &configuration,
 
     server.on(
         "/submitconfig", HTTP_POST,
-        [&configuration, submitFunc, this](AsyncWebServerRequest *request) {
-            if (request->params() == 0) {
-                DEBUG_PRINTLN(
-                    "Refusing to take over an empty configuration submission.");
-                request->send(500);
-                return;
-            }
-            debugPrintRequest(request);
-
-            for (int i = 0; i < request->params(); i++) {
-                AsyncWebParameter *webParameter = request->getParam(i);
-                if (webParameter->isPost() &&
-                    webParameter->value().length() != 0) {
-                    configuration.set(webParameter->name().c_str(),
-                                      webParameter->value().c_str());
-                }
-            }
-
-            configuration.save();
-            request->send(201);
-
-            // Only call submitFunc when it has been set to something useful
-            if (submitFunc)
-                submitFunc();
-        });
-    server.on(
-        "/submitfeedingtime", HTTP_POST,
         [&configuration, submitFunc, this](AsyncWebServerRequest *request) {
             if (request->params() == 0) {
                 DEBUG_PRINTLN(
